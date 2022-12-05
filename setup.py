35ecbc8987dabb79fc81eb3d74bb6b8aa3b3763d
jayanams <119804723+jayanams@users.noreply.github.com>
2022-12-05 01:26:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from distutils.core import setup, Extension
-setup
-(name='foo',
-      version='1.0',
-      ext_modules=[Extension('foo', ['foo.c'])],
-      )
+setup (name='foo',
+       version='1.0',
+       ext_modules=[Extension('foo', ['foo.c'])],)
